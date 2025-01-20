90d454ba50712b566696001a39ef914bf6d5ef68
Otto <1259236879@qq.com>
2025-01-21 00:04:24 +0800

    Refactor feature extraction class and update report files: rename class, adjust table of contents, and regenerate auxiliary files

--- libs/utils/rgb_pos_feature.py
+++ libs/utils/rgb_pos_feature.py
@@ -3,10 +3,9 @@ import torch.nn as nn
 
 
 
-class FeatureExtractor(nn.Module):
+class RGBandPOSFeature(nn.Module):
     def __init__(self):
         super().__init__()
-    
     
     def forward(self, x):
         """
